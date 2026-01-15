5017c78eb26039f86c7b98480a7ba7a05fed2826
Snigdha Srivastava <snigdha21csu283@ncuindia.edu>
2026-01-15 16:45:34 +0530

    Remove embedded backend

--- planet-nakshatra-backend/src/testimonials/testimonials.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestimonialsController } from './testimonials.controller';
-import { TestimonialsService } from './testimonials.service';
-
-@Module({
-  controllers: [TestimonialsController],
-  providers: [TestimonialsService],
-})
-export class TestimonialsModule {}
